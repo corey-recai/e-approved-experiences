06e96d09e021c2ced6844f0b2f5c08c61f505cee
coreyrecai <coreycrecai@gmail.com>
2022-12-08 23:03:41 -0600

    add villas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ItemPage } from "./views/ItemPage";
 import cars from "./data/cars.json";
 import yachts from "./data/yachts.json";
+import villas from "./data/villas.json";
 
 function App() {
   return (
@@ -26,12 +27,25 @@ function App() {
           path='/luxury-yachts'
           element={
             <ItemPage
-              title='Miami &amp; Bahamas Yacht Charters'
+              title='Miami &amp; Bahamas Yacht CHARTERS'
               media={{
                 type: "image",
                 link: "https://static.wixstatic.com/media/6da91d_7388e353a8b346168cd9a1e4f9e23afd~mv2.jpg/v1/fill/w_1504,h_399,fp_0.50_0.55,q_85,enc_auto/banner1_1593c2fb-08a8-4f9a-ae3d-c43a6ba987f5.jpg",
               }}
               data={yachts}
+            />
+          }
+        />
+        <Route
+          path='/luxury-villas'
+          element={
+            <ItemPage
+              title='Miami Luxury Villa Rentals'
+              media={{
+                type: "image",
+                link: "https://static.wixstatic.com/media/6da91d_4259bbab3c914d26bfed4c83b358c6fe~mv2.jpg/v1/fill/w_1885,h_500,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/6da91d_4259bbab3c914d26bfed4c83b358c6fe~mv2.jpg",
+              }}
+              data={villas}
             />
           }
         />
